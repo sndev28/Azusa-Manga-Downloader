e076626e52075bc3d6e86479de35be63c5b64b63
sndev28 <niyas.shamil@gmail.com>
2021-05-16 17:07:43 +0530

    Material UI first commit. Working but not exe compilable

--- azusa_gui.py
+++ azusa_gui.py
@@ -6,7 +6,7 @@
                                              #                                                                             #
                                              #                                 AZUSA                                       #
                                              #                                                                             #
-                                             #                        VERSION CODE : 3.87.54                               #
+                                             #                        VERSION CODE : 4.00.00                               #
                                              #                                                                             #
                                              #                                                                             #
                                              #                                                                             #
@@ -49,7 +49,7 @@ from retrievers.py_exe import resource_path
 
 
 #version code
-version_code = '3.87.54'
+version_code = '4.00.00'
 
 #Runner
 
@@ -58,25 +58,25 @@ def main():
 
     multiprocessing.freeze_support()
     
-    from kivy.app import App
-    from kivy.uix.widget import Widget
+    from kivymd.app import MDApp
     from kivy.lang import Builder
     from kivy.core.window import Window
     from kivy.clock import Clock
     from kivy.core.audio import SoundLoader
     from kivy.resources import resource_add_path, resource_find
 
+
     if hasattr(sys, '_MEIPASS'):
         resource_add_path(path.join(sys._MEIPASS))
 
     Window.size = (950,750)
 
-    Builder.load_file(resource_path('azusa.kv'))
+    # Builder.load_file(resource_path('azusa.kv'))
     alert = SoundLoader.load(resource_path('alert.mp3'))
 
 
 
-    class ProgramWindow(Widget):
+    class Application(MDApp):
 
         
 
@@ -84,14 +84,13 @@ def main():
 
             print("Starting process!")
 
-            self.ids.batch_download.disabled = True  #disables button to prevent extra initialization
-            self.ids.download_status.text = 'Initializing Download...Please wait!'
-            self.ids.download_progress.value = 0
-            self.ids.directory.readonly = True
-            self.ids.url.readonly = True
-            self.ids.cpu_count.readonly = True
-            self.ids.cancel_download.disabled = False
-            self.ids.serialize_check.disabled = True
+            self.root.ids.batch_download.disabled = True  #disables button to prevent extra initialization
+            self.root.ids.download_status.text = 'Initializing Download...Please wait!'
+            self.root.ids.download_progress.value = 0
+            self.root.ids.directory.readonly = True
+            self.root.ids.url.readonly = True
+            self.root.ids.cancel_download.disabled = False
+            self.root.ids.serialize_check.disabled = True
 
             
 
@@ -103,47 +102,69 @@ def main():
 
             t1 = time.perf_counter()
 
+            if self.root.ids.url.text == '':
+                self.root.ids.url.helper_text = 'Empty url!'
+                self.root.ids.url.error = True
+                self.root.ids.download_status.text = 'Empty url!'
+                self.root.ids.batch_download.disabled = False #renables button due to error
+                self.root.ids.directory.readonly = False
+                self.root.ids.url.readonly = False
+                self.root.ids.cancel_download.disabled = True
+                self.root.ids.serialize_check.disabled = False
+                return
+
+            elif self.root.ids.directory.text == '':
+                self.root.ids.directory.helper_text = 'Empty directory!'
+                self.root.ids.directory.error = True
+                self.root.ids.download_status.text = 'Empty directory!'
+                self.root.ids.batch_download.disabled = False #renables button due to error
+                self.root.ids.directory.readonly = False
+                self.root.ids.url.readonly = False
+                self.root.ids.cancel_download.disabled = True
+                self.root.ids.serialize_check.disabled = False
+                return
+
+
             try:
-                test = BeautifulSoup(requests.get(self.ids.url.text).text, 'lxml')
+                test = BeautifulSoup(requests.get(self.root.ids.url.text).text, 'lxml')
             except:
-                self.ids.url.text = 'Invalid url!'
-                self.ids.batch_download.disabled = False #renables button due to error
-                self.ids.directory.readonly = False
-                self.ids.url.readonly = False
-                self.ids.cpu_count.readonly = False
-                self.ids.cancel_download.disabled = True
-                self.ids.serialize_check.disabled = False
-                self.ids.download_status.text = 'Download not started yet!'
+                self.root.ids.url.error = True
+                self.root.ids.url.helper_text = 'Invalid url!'
+                self.root.ids.batch_download.disabled = False #renables button due to error
+                self.root.ids.directory.readonly = False
+                self.root.ids.url.readonly = False
+                self.root.ids.cancel_download.disabled = True
+                self.root.ids.serialize_check.disabled = False
+                self.root.ids.download_status.text = 'Download not started yet!'
                 return
 
-            if not urlchecker(self.ids.url.text):
-                self.ids.url.text = 'Unsupported site!'
-                self.ids.batch_download.disabled = False #renables button due to error
-                self.ids.directory.readonly = False
-                self.ids.url.readonly = False
-                self.ids.cpu_count.readonly = False
-                self.ids.cancel_download.disabled = True
-                self.ids.serialize_check.disabled = False
-                self.ids.download_status.text = 'Download not started yet!'
+            if not urlchecker(self.root.ids.url.text):
+                self.root.ids.url.error = True
+                self.root.ids.url.helper_text = 'Unsupported site!'
+                self.root.ids.batch_download.disabled = False #renables button due to error
+                self.root.ids.directory.readonly = False
+                self.root.ids.url.readonly = False
+                self.root.ids.cancel_download.disabled = True
+                self.root.ids.serialize_check.disabled = False
+                self.root.ids.download_status.text = 'Download not started yet!'
                 return
 
-            if not path.exists(self.ids.directory.text):
-                self.ids.directory.text = 'Invalid directory!'
-                self.ids.batch_download.disabled = False #renables button due to error
-                self.ids.directory.readonly = False
-                self.ids.url.readonly = False
-                self.ids.cpu_count.readonly = False
-                self.ids.cancel_download.disabled = True
-                self.ids.serialize_check.disabled = False
-                self.ids.download_status.text = 'Download not started yet!'
+            if not path.exists(self.root.ids.directory.text):
+                self.root.ids.directory.error = True
+                self.root.ids.batch_download.disabled = False #renables button due to error
+                self.root.ids.directory.readonly = False
+                self.root.ids.url.readonly = False
+                self.root.ids.cancel_download.disabled = True
+                self.root.ids.serialize_check.disabled = False
+                self.root.ids.download_status.text = 'Download not started yet!'
                 return
             else:
-                if self.ids.directory.text[-1] == '\\':
-                    self.ids.directory.text = self.ids.directory.text[:-1]
+                if self.root.ids.directory.text[-1] == '\\':
+                    self.root.ids.directory.text = self.root.ids.directory.text[:-1]
 
             
             #Pacman gif starts
-            self.ids.gif.opacity = 1
+            self.root.ids.gif.opacity = 1
 
 
             downloader(self, self.serialize_flag)
@@ -152,22 +173,22 @@ def main():
             #uncomment to view execution time details
             total_download_time = time.perf_counter() - t1
             print('Execution time = ', total_download_time,'s')
-            self.ids.download_status.text += f' Total time taken = {time.strftime("%H:%M:%S", time.gmtime(total_download_time))}'
+            self.root.ids.download_status.text += f' Total time taken = {time.strftime("%H:%M:%S", time.gmtime(total_download_time))}'
 
         def updater_button(self):
-            pool = concurrent.futures.ThreadPoolExecutor(max_workers=1)
+            pool = ThreadPoolExecutor(max_workers=1)
             pool.submit(self.updater)
 
         def updater(self):
 
-            self.ids.download_status.text = 'Checking for update!'
+            self.root.ids.download_status.text = 'Checking for update!'
             new_version_code = requests.get('https://drive.google.com/uc?export=download&id=1BKHZ1c_S6xVWwf_LE_prBqGa8X3M6BVH').text
 
             if new_version_code != version_code:
-                self.ids.download_status.text = 'Downloading update!'
+                self.root.ids.download_status.text = 'Downloading update!'
                 update_link = requests.get('https://drive.google.com/uc?export=download&id=1WJaXgcxv2tnn0WCtXiHjLQCET5c10bF_').text
 
-                self.ids.gif.opacity = 1
+                self.root.ids.gif.opacity = 1
 
                 download = requests.get(update_link)
 
@@ -177,46 +198,43 @@ def main():
                         file.write(chunk)
 
 
-                self.ids.download_status.text = 'Updating!'
+                self.root.ids.download_status.text = 'Updating!'
 
-                self.ids.gif.opacity = 0
+                self.root.ids.gif.opacity = 0
 
-                self.ids.download_status.text = 'Please restart the application! Delete the older version!'
+                self.root.ids.download_status.text = 'Please restart the application! Delete the older version!'
                 
 
             else:
-                self.ids.download_status.text = 'No available update!'
+                self.root.ids.download_status.text = 'No available update!'
 
 
         def cancel_download(self):
             sys.exit(0)
 
-            
         serialize_flag = False
-        
-        def serialize(self, instance, value):
-            self.serialize_flag = value
+
+        def serialize(self):
+            if self.root.ids.serialize_check.current:
+                self.serialize_flag = self.root.ids.serialize_check.current = False
+                self.root.ids.serialize_check.md_bg_color = 1,1,1,1
+                self.root.ids.serialize_check.text_color = self.theme_cls.primary_color
+
+            else:
+                self.serialize_flag = self.root.ids.serialize_check.current = True
+                self.root.ids.serialize_check.md_bg_color = self.theme_cls.primary_color
+                self.root.ids.serialize_check.text_color = 1,1,1,1
+
             print(self.serialize_flag)
 
-        def cpu_counter(self, *args):
-            self.ids.cpu_count.text = str(args[1])
 
-
-        
-        
-
-
-
-
-
-            
-
-
-
-    class Application(App):
         def build(self):
+            self.theme_cls.primary_palette = "Green"
+            self.theme_cls.theme_style = "Light"
             self.title = 'Azusa - Manga Downloader'
-            return ProgramWindow()
+            self.root = Builder.load_file(resource_path('azusa.kv'))
+
+        
 
 
     Application().run()
